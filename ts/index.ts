f968e76670892be056d323b9b7b02bfa7a2aa89c
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-02-22 17:58:03 +0100

    feat(ux): no reload when changing page

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+function setupEvents() {
+  document.querySelectorAll("a").forEach((el) => {
+    if (!el.href.startsWith(window.location.origin)) return;
+    if (el.href == window.location.origin + "/random") return;
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      // change page
+      fetch(el.href)
+        .then((resp) => resp.text())
+        .then((html) => {
+          const doc = new DOMParser().parseFromString(html, "text/html");
+          window.history.pushState({}, "", el.href);
+          document.title = doc.title;
+          document.body = doc.body;
+          setupEvents();
+          scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        });
+    });
+  });
+}
+
+window.addEventListener("popstate", (_) => {
+  window.location.reload();
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+setupEvents();
