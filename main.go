a8bf2045abb8a25904f811ca6af9fd1037f74ffd
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-02-22 13:07:31 +0100

    feat(golatt): setup base

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"embed"
+	"flag"
+	"github.com/anhgelus/golatt"
+)
 
+//go:embed templates
+var templates embed.FS
+
+var g *golatt.Golatt
+
+var dev bool
+
+func init() {
+	flag.BoolVar(&dev, "dev", false, "Run in development mode")
+}
+
+func main() {
+	flag.Parse()
+	g = golatt.New(templates)
+	g.DefaultSeoData = &golatt.SeoData{
+		Image:       "",
+		Description: "",
+		Domain:      "ring.nouveauprintemps.org",
+	}
+	g.FormatTitle = func(t string) string {
+		return t + " - Webring - Nouveau Printemps"
+	}
+	g.Templates = append(g.Templates,
+		"templates/base/*.gohtml",
+	)
+	if dev {
+		g.StartServer(":8000")
+	} else {
+		g.StartServer(":80")
+	}
 }
